af11843fdc2345f7c8c2a34ab7c81791cf52be21
Cassandra Whitty <cassandraallexwhitty@gmail.com>
2020-10-17 22:46:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -24,7 +24,7 @@ def nyc_pigeon_organizer(data)
       value.each do |inner_key, names|
         names.each do |name|
           binding.pry
-          if 
+          if !final_array[name]
         end
       end
   final_array
