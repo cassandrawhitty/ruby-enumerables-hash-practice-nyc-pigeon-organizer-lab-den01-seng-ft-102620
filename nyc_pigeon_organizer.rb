9ac0310f363d800871f3fa490a5d030b4bf3ed39
Cassandra Whitty <cassandraallexwhitty@gmail.com>
2020-10-17 21:43:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -27,6 +27,7 @@ def nyc_pigeon_organizer(data)
   names_1 = names[0][0]
   names_2 = names[0][1]
   names = names_1.concat(names_2)
+  hash = Hash[names.collect {|item| [item, ""]}]
   binding.pry
 end
 
